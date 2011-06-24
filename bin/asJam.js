eadaade3b9b17a186bee9ac395e37b360eb2e2dc
Matt Glazar <matt.glazar@gmail.com>
2011-06-23 21:46:48 -0700

    Simple code regurgitat0r

---
+++ bin/asJam.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var parser = require('../lib/parse');
+var printer = require('../lib/print');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+    var ast = parser.parse(data);
+    var code = printer.gen_code(ast, { beautify: true });
+
+    console.log(code);
+});
