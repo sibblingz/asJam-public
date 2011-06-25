b2d70a74bb4c0e9df78659bec2cc7510c2902aa6
Matt Glazar <matt.glazar@gmail.com>
2011-06-24 18:16:43 -0700

    Implicit this calls in subscopes

---
+++ testData/implicit-this.js
@@ -0,0 +1,1 @@
+var Foobar=Class.create({methods:{one:function one(){var self=this;self.two()},two:function two(){}}})
---
+++ testData/implicit-this2.js
@@ -0,0 +1,1 @@
+var Foobar=Class.create({methods:{one:function one(){var self=this;setTimeout(function(){self.two()},0)},two:function two(){}}})
