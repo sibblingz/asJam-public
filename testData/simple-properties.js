563ea387ddded8f6efe6662d8b6dd5f80f5c4459
Matt Glazar <matt.glazar@gmail.com>
2011-06-27 14:20:35 -0700

    Rewrite properties properly

--- testData/simple-properties.js
+++ testData/simple-properties.js
@@ -1,1 +1,1 @@
-var MyClass=Class.create({constructor:function MyClass(){this.prop=42;this.mc=new MovieClip}})
+var MyClass=Class.create({constructor:function MyClass(){this.mc=new MovieClip},properties:{notSet:undefined,prop:42+9,mc:undefined}})
---
+++ testData/static-method.js
@@ -0,0 +1,1 @@
+var Foobar=Class.create({statics:{myStatic:function myStatic(){},_privateStatic:function _privateStatic(){},ps:function ps(){}}})
