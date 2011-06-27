69d2055ab18285dd8c8bf174e08df07344b19705
Matt Glazar <matt.glazar@gmail.com>
2011-06-27 14:24:53 -0700

    Use null instead of undefined for unset properties

--- testData/simple-properties.js
+++ testData/simple-properties.js
@@ -1,1 +1,1 @@
-var MyClass=Class.create({constructor:function MyClass(){this.mc=new MovieClip},properties:{notSet:undefined,prop:42+9,mc:undefined}})
+var MyClass=Class.create({constructor:function MyClass(){this.mc=new MovieClip},properties:{notSet:null,prop:42+9,mc:null}})
