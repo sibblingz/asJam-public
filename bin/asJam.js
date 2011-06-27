653648d46acb6c0dd364c5e4989d1f04d06f2686
Matt Glazar <matt.glazar@gmail.com>
2011-06-27 11:41:57 -0700

    Have asJam binary perform AS3=>JS rewrite

--- bin/asJam.js
+++ bin/asJam.js
@@ -4,6 +4,7 @@ var printer = require('../lib/print');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
     var ast = parser.parse(data);
+    ast = printer.rewrite(ast);
     var code = printer.gen_code(ast, { beautify: true });
 
     console.log(code);
