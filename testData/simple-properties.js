c070d20b5dbaf1ddd1bd066fb02e2a5531dbc3b5
Matt Glazar <matt.glazar@gmail.com>
2011-07-05 11:50:46 -0700

    Accept const properties

--- testData/simple-properties.js
+++ testData/simple-properties.js
@@ -1,1 +1,1 @@
-var MyClass=Class.create({constructor:function MyClass(){this.mc=new MovieClip},properties:{notSet:null,prop:42+9,mc:null}})
+var MyClass=Class.create({constructor:function MyClass(){this.mc=new MovieClip},properties:{notSet:null,prop:42+9,mc:null,whatever:9}})
