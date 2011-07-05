9b1a0ab66e24343204e3d46beb83f8704bcc406e
Matt Glazar <matt.glazar@gmail.com>
2011-07-05 16:36:29 -0700

    Fix crash for unset static properties

--- testData/static-property.js
+++ testData/static-property.js
@@ -1,1 +1,1 @@
-var MyClass=Class.create({statics:{asdf:42,mc:new MovieClip,foobar:{get:function get_foobar(){return 42},set:function set_foobar(value){}}}})
+var MyClass=Class.create({statics:{asdf:42,mc:new MovieClip,unset:null,foobar:{get:function get_foobar(){return 42},set:function set_foobar(value){}}}})
