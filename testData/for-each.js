5818730c55bf0efb1c8b8ba7f82665b6630f26a9
Matt Glazar <matt.glazar@gmail.com>
2011-07-05 12:39:29 -0700

    Rewrite for-each loops

---
+++ testData/for-each.js
@@ -0,0 +1,1 @@
+for(var key in[2,6,3]){if(!{}.hasOwnProperty.call([2,6,3],key))continue;var x=[2,6,3][key];if(x>2){break}}var y;for(var keya in z){if(!{}.hasOwnProperty.call(z,keya))continue;y=z[keya]}
